16ec74421a21e75c85d34c4fd70361630185f3b6
aitorzubiri <aitorgaillard99@gmail.com>
2025-10-02 13:42:36 +0200

    ajustar imports

--- app/src/main/java/com/visibility/rcw_visibility/presentation/MainActivity.kt
+++ app/src/main/java/com/visibility/rcw_visibility/presentation/MainActivity.kt
@@ -3,17 +3,26 @@ package com.visibility.rcw_visibility.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.*
-import androidx.wear.compose.material.*
-import com.visibility.rcw_visibility.R
-import com.visibility.rcw_visibility.presentation.theme.Rcw_visibilityTheme
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
+import com.visibility.rcw_visibility.presentation.theme.Rcw_visibilityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
